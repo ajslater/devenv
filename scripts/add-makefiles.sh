04a0cb476a165f98e9b53044d35ec2d29006ac3d
AJ Slater <aj@slater.net>
2026-03-02 18:51:30 -0800

    add cli arg for add-makefiles features

--- scripts/add-makefiles.sh
+++ scripts/add-makefiles.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 # copy specific makefiles
 set -euo pipefail
-set -x
 DEVENV_SRC=${DENENV_SRC:-$(realpath "$(dirname "$0")/..")}
+DEFAULT_FEATURES=(common node_root python)
+if [[ ${#@} -gt 0 ]]; then
+  FEATURES=("$@")
+else
+  FEATURES=("${DEFAULT_FEATURES[@]}")
+fi
 
-for feature in "$@"; do
+for feature in "${FEATURES[@]}"; do
   export "DEVENV_${feature^^}=1"
 done
 
--- scripts/init-project.sh
+++ scripts/init-project.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 mkdir -p bin
 DEVENV_SRC=${DENENV_SRC:-$(realpath "$(dirname "$0")/..")}
 export DEVENV_COMMON=1
+"$DEVENV_SRC"/scripts/add-makefiles.sh "$@"
 "$DEVENV_SRC"/scripts/copy-new-files.sh "$DEVENV_SRC"/bin bin
 "$DEVENV_SRC"/scripts/copy-new-files.sh "$DEVENV_SRC"/init .
 uv pip install semver tomlkit
