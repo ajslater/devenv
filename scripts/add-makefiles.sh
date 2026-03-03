4af43ef1038667fab7760165ea98dd53b4a17529
AJ Slater <aj@slater.net>
2026-03-02 21:57:01 -0800

    add help to add-makefiles.sh

--- scripts/add-makefiles.sh
+++ scripts/add-makefiles.sh
@@ -2,12 +2,23 @@
 # copy specific makefiles
 set -euo pipefail
 DEVENV_SRC=${DENENV_SRC:-$(realpath "$(dirname "$0")/..")}
+
+while getopts "h?:" opt; do
+  case "$opt" in
+    h | \?)
+      source "$DEVENV_SRC"/scripts/all-features.sh
+      echo Features: "${ALL_FEATURES[@]}"
+      exit 1
+      ;;
+  esac
+done
+
 if [[ ${#@} -gt 0 ]]; then
   FEATURES=("$@")
 else
   FEATURES=(common node_root python)
 fi
-
+echo Adding features: "${FEATURES[@]}"
 for feature in "${FEATURES[@]}"; do
   export "DEVENV_${feature^^}=1"
 done
