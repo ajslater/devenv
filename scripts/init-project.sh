a1c8e2bb2748742faa908c3fd67646846e9e1309
AJ Slater <aj@slater.net>
2026-03-04 16:14:05 -0800

    fix init script

--- scripts/init-project.sh
+++ scripts/init-project.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 # Initialize a new project
 set -euo pipefail
+PD=$PWD
 mkdir -p bin
 DEVENV_SRC=${DENENV_SRC:-$(realpath "$(dirname "$0")/..")}
-export DEVENV_COMMON=1
-"$DEVENV_SRC"/scripts/add-makefiles.sh "$@"
-"$DEVENV_SRC"/scripts/copy-new-files.sh "$DEVENV_SRC"/bin bin
-"$DEVENV_SRC"/scripts/copy-new-files.sh "$DEVENV_SRC"/init .
-uv pip install semver tomlkit
+source "$DEVENV_SRC"/scripts/add-makefiles.sh "$@"
+if [[ ${DEVENV_PYTHON:-} ]]; then
+  uv init
+fi
+"$DEVENV_SRC"/scripts/copy-new-files.sh "$DEVENV_SRC"/bin "$PD"/bin
+"$DEVENV_SRC"/scripts/copy-new-files.sh "$DEVENV_SRC"/init "$PD"
+uv pip install packaging semver tomlkit
 "$DEVENV_SRC/scripts/update-devenv.sh"
