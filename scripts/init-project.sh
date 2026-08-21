6b966d785131998883e98b38deab436d3c24acfe
AJ Slater <aj@slater.net>
2026-08-21 09:32:55 -0700

    update eslint

--- scripts/init-project.sh
+++ scripts/init-project.sh
@@ -21,4 +21,5 @@ fi
 uv run "$DEVENV_SRC"/scripts/copy_files.py "$PD" --root "$DEVENV_SRC"/init
 mv eslint.config.init.js eslint.config.js
 uv pip install packaging pathspec semver tomlkit mbake
+bun init
 uv run "$DEVENV_SRC/scripts/update_devenv.py"
